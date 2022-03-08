40a10ac5a3578b6186eb6549b2ac793043dfd542
alonzo-david <alonzovid@gmail.com>
2022-03-07 21:55:17 -0600

    Commit 3
    Validacion de js

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const mousetrap = require("mousetrap");
+let formulario = document.getElementById('form-login');
+
+//const usuariosBloqueados = ["Saab", "Volvo", "BMW"];
+
+
+formulario.addEventListener('submit', function(evento){
+    
+    evento.preventDefault();
+
+    let usuario = document.getElementById("user").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let fechaNac = document.getElementById("fechanac").value;
+
+    alert('Bienvenido ' + usuario);
+});
 
 
