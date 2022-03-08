b0036f6099141f0197cacc7d17710d79c263cca2
alonzo-david <alonzovid@gmail.com>
2022-03-07 22:15:37 -0600

    Validacion
    Validaciones con el label error y seccion de puntos extras.

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 let formulario = document.getElementById('form-login');
 
-//const usuariosBloqueados = ["Saab", "Volvo", "BMW"];
+const usuariosBloqueados = ["dalo", "dalo2", "dalo2"];
+
 
 
 formulario.addEventListener('submit', function(evento){
     
     evento.preventDefault();
 
-    let usuario = document.getElementById("user").value;
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let fechaNac = document.getElementById("fechanac").value;
+    let bloqueado = false;
+    let usuario = document.getElementById("user");
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let fechaNac = document.getElementById("fechanac");
+    let lblError = document.getElementById("error");
 
-    alert('Bienvenido ' + usuario);
+    if(usuario.value == ""){
+        lblError.innerText = 'El campo usuario no puede estar vacio';
+        usuario.focus;
+        return;
+    }
+    if(email.value == ""){
+        lblError.innerText = 'El campo email no puede estar vacio';
+        email.focus;
+        return;
+    }
+    if(password.value == ""){
+        lblError.innerText = 'El campo password no puede estar vacio';
+        password.focus;
+        return;
+    }
+    if(fechaNac.value == ""){
+        lblError.innerText = 'El campo Fecha Nacimiento no puede estar vacio';
+        fechaNac.focus;
+        return;
+    }
+
+    usuariosBloqueados.forEach( function(valor, indice, array) {
+        console.log("En el índice " + indice + " hay este valor: " + valor);
+        if(valor == usuario.value){
+            bloqueado = true;
+        }
+    });
+
+    if(bloqueado){
+        alert('Usuario ' + usuario.value + ' bloqueado');
+        usuario.focus;
+    }else{
+        alert('Bienvenido ' + usuario.value);
+    }
+    
 });
 
 
--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
   const ventana = new BrowserWindow({
     width: 475,
-    height: 450,
+    height: 475,
     webPreferences: {
       nodeIntegration: true,
 
